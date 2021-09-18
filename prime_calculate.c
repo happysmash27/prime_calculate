ab4ad78a2d8eb69687489ef2b057c85d8aab4923
happysmash27 <happysmash27@protonmail.com>
2021-09-17 23:46:42 -0700

    Fixed bugs

--- prime_calculate.c
+++ prime_calculate.c
@@ -6,28 +6,28 @@
 int is_prime_u(unsigned int num){
      for (unsigned int i = 2; i<=num/2; i++){
 	  if (num%i == 0){
-	       return 1;
+	       return 0;
 	  }
      }
-     return 0;
+     return 1;
 }
 
 int is_prime_ul(unsigned long num){
      for (unsigned long i = 2; i<=num/2; i++){
 	  if (num%i == 0){
-	       return 1;
+	       return 0;
 	  }
      }
-     return 0;
+     return 1;
 }
 
 int is_prime_ull(unsigned long long num){
      for (unsigned long long i = 2; i<=num/2; i++){
 	  if (num%i == 0){
-	       return 1;
+	       return 0;
 	  }
      }
-     return 0;
+     return 1;
 }
 
 int main(int argc, char** argv){
@@ -51,28 +51,30 @@ int main(int argc, char** argv){
 	  //Unsigned int
 	  if (max_num<=UINT_MAX){
 	       unsigned int max_num_sm = max_num;
-	       for (unsigned int i=1; i<max_num_sm; i++){
+	       for (unsigned int i=1; i<=max_num_sm; i++){
 		    if(is_prime_u(i)){
-			 fprintf(stdout, "%u\n", max_num_sm);
+			 fprintf(stdout, "%u\n", i);
 		    }
 	       }
+	       return 0;
 	  }
 
 	  //Unsigned long int
 	  unsigned long max_num_sm = max_num;
 	  for (unsigned long i=1; i<= max_num_sm; i++){
 	       if(is_prime_ul(i)){
-		    fprintf(stdout, "%lu\n", max_num_sm);
+		    fprintf(stdout, "%lu\n", i);
 	       }
 	  }
-	  
+	  return 0;
      } else {
 	  //Unsigned long long int
 	  for (unsigned long long i=1; i<= max_num; i++){
 	       if(is_prime_ull(i)){
-		    fprintf(stdout, "%llu\n", max_num);
+		    fprintf(stdout, "%llu\n", i);
 	       }
 	  }
+	  return 0;
      }
 }
 	       
